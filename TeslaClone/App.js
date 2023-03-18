b7e971b5982689290d0f95e071c3428402ae9eeb
Gurpreet1199 <gurdaspur9@gmail.com>
2023-03-18 13:44:36 +0530

    images added

---
+++ TeslaClone/App.js
@@ -0,0 +1,46 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View ,ImageBackground} from 'react-native';
+import React from 'react';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.carContainer}>
+    <ImageBackground
+     source={require('./assets/images/ModelX.jpeg')}
+    />
+      <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subtitle}>Starting at 69000$</Text>
+      </View>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  carContainer: {
+    width:'100%',
+    height:'100%',
+  },
+  titles: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center',
+  },
+  title :{
+    fontSize: 40,
+    fontWeight: '500',
+
+  },
+  subtitle: {
+    fontSize:16,
+    color: '#5c5e62',
+  }
+});
