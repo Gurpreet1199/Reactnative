8c4f4c8d9116227e6b9d6e1e977cc6390758d380
Gurpreet1199 <gurdaspur9@gmail.com>
2024-02-22 16:04:16 +0530

    gesture handling

--- TinderClone/App.js
+++ TinderClone/App.js
@@ -1,13 +1,44 @@
+import 'react-native-gesture-handler';
 import React from "react";
-import {Text,Image, ImageBackground,View, StyleSheet} from 'react-native';
+import {Text,Image, ImageBackground,View, StyleSheet, Pressable} from 'react-native';
 import Card from "./src/components/TinderCard";
 import users from "./assets/data/users";
-
+import Animated, { useAnimatedGestureHandler, useSharedValue,useAnimatedStyle,withSpring } from 'react-native-reanimated';
+import { PanGestureHandler ,GestureHandlerRootView} from 'react-native-gesture-handler';
 const App= () =>{
+    const translateX=useSharedValue(0);
+    const cardStyle=useAnimatedStyle(()=>({
+        transform:[{
+            translateX:translateX.value,
+        }]
+    }));
+    const gestureHandeler=useAnimatedGestureHandler({
+        onStart:(_,context)=>{
+            context.startX=translateX.value
+        },
+        onActive:(event,context) =>{
+            translateX.value=context.startX+event.translationX
+        },
+        onEnd:()=>{
+            console.warn('Touch ended');
+        },
+    });
     return (
-        <View style={styles.pageContainer}>
-            <Card user={users[0]}/>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <View style={styles.pageContainer}>
+            <PanGestureHandler onGestureEvent={gestureHandeler}>
+            <Animated.View style={[styles.animatedCard,cardStyle]}>
+                <Card user={users[0]}/>
+            </Animated.View>
+            </PanGestureHandler>
+
+            <Pressable
+            onPress={()=>(sharedValue.value=withSpring(Math.random()))}>
+                    <Text>Change Value</Text>
+            </Pressable>
         </View>        
+        </GestureHandlerRootView>
+
         );
 }
 const styles=StyleSheet.create({
@@ -15,6 +46,11 @@ const styles=StyleSheet.create({
         flex:1,
         alignItems:'center',
         justifyContent:'center'
+    },
+    animatedCard:{
+        width:'100%',
+        justifyContent:'center',
+        alignItems:'center',
     }
 });
 export default App;
