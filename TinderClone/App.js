2f190d84c49440d3411613d3e8ae8b6b034b6107
Gurpreet1199 <gurdaspur9@gmail.com>
2024-02-22 12:51:55 +0530

    Custom tinder component variable

--- TinderClone/App.js
+++ TinderClone/App.js
@@ -1,59 +1,20 @@
 import React from "react";
 import {Text,Image, ImageBackground,View, StyleSheet} from 'react-native';
+import Card from "./src/components/TinderCard";
+import users from "./assets/data/users";
+
 const App= () =>{
     return (
         <View style={styles.pageContainer}>
-            <View style={styles.card}>
-                <ImageBackground source={{uri:"https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG"}} 
-                    style={styles.image}>
-                    <View style={styles.cardinner}>
-                        <Text style={styles.name}>Elon Musk</Text>
-                        <Text style={styles.bio}>In the middle of the night i built a rocket to make earth people move to mars</Text>
-                    </View>
-                </ImageBackground>
-            </View>
+            <Card user={users[0]}/>
         </View>        
         );
 }
 const styles=StyleSheet.create({
-    cardinner:{
-        padding:10,
-        //backgroundColor:'red'
-    },
-    name:{
-        fontSize:30,
-        color:'white',
-        fontWeight:'bold',
-    },
-    bio:{
-        fontSize:18,
-        color:'white',
-        lineHeight:25,
-    },
     pageContainer:{
         flex:1,
         alignItems:'center',
         justifyContent:'center'
-    },
-    image:{
-        width:'100%',
-        height:'100%',
-        borderRadius:10,
-        overflow:'hidden',
-        justifyContent:'flex-end'
-    },
-    card:{
-        width:'95%',
-        height:'70%',
-        borderRadius:10,
-        shadowColor: "#000000",
-        shadowOffset: {
-            width: 0,
-            height: 3,
-        },
-        shadowOpacity:  0.7,
-        shadowRadius: 3.05,
-        elevation: 4
     }
-})
+});
 export default App;
---
+++ TinderClone/src/components/TinderCard/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Text ,View,ImageBackground,StyleSheet} from "react-native";
+const Card =(props)=>{
+    const {name,image,bio}=props.user;
+    return (
+        <View style={styles.card}>
+                <ImageBackground source={{uri:image}} 
+                    style={styles.image}>
+                    <View style={styles.cardinner}>
+                        <Text style={styles.name}>{name}</Text>
+                        <Text style={styles.bio}>{bio}</Text>
+                    </View>
+                </ImageBackground>
+            </View>
+    );
+}
+const styles=StyleSheet.create({
+    cardinner:{
+        padding:10,
+        //backgroundColor:'red'
+    },
+    name:{
+        fontSize:30,
+        color:'white',
+        fontWeight:'bold',
+    },
+    bio:{
+        fontSize:18,
+        color:'white',
+        lineHeight:25,
+    },
+    image:{
+        width:'100%',
+        height:'100%',
+        borderRadius:10,
+        overflow:'hidden',
+        justifyContent:'flex-end'
+    },
+    card:{
+        width:'95%',
+        height:'70%',
+        borderRadius:10,
+        shadowColor: "#000000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity:  0.7,
+        shadowRadius: 3.05,
+        elevation: 4
+    }
+});
+export default Card;
